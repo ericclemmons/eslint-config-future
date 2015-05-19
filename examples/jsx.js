8f49453122462b95ebeb25c2b63e2cf8d73d34e1
Eric Clemmons <eric@smarterspam.com>
2015-05-18 22:01:32 -0500

    Initial examples

---
+++ examples/jsx.js
@@ -0,0 +1,11 @@
+import React from "util";
+
+export default class MyComponent extends React.Component {
+  displayName = "MyComponent"
+
+  render() {
+    return (
+      <h1>Howdy!</h1>
+    );
+  }
+}
